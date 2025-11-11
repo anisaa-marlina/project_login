f64f4096eb9e0c96ba0ff4ed6754db2d4028d6a6
anisaa-marlina <anisaamarlinaa@gmail.com>
2025-11-11 14:06:31 +0700

    Menambahkan project login dan img

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+</head>
+<body>
+    <h2>Form Login</h2>
+    <Form method="post">
+        Username: <input type= "text" name="username" requeired><br><br>
+        Password: <input type= "password" name="password" requeired><br><br>
+        <button type="submit">Login</buton>
+        <button type="reset">Batal</buton>
+</Form>
+</body>
+</html>
